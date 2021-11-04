9606186989c9a8aba59e0020fad418530a4f492e
BastianL2 <93203724+BastianL2@users.noreply.github.com>
2021-11-03 21:32:52 -0300

    Add files via upload

---
+++ FizzBuzz2.py
@@ -0,0 +1,11 @@
+#Mi FizzBuzz
+numero = int(input("Indique un valor entero: "))
+salida = ""
+if numero %3 == 0:
+    salida += "Fizz"
+if numero %5 == 0:
+    salida += "Buzz"
+if salida == "":
+    salida = str(numero)
+print(salida)
+
